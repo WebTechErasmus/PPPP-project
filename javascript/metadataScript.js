4b33f42df3bb8cb238f53bf541d959442badd91e
Maud <johansson.maud98@gmail.com>
2022-05-05 18:38:13 +0200

    kan nå klikke deg til riktig sted i teksten ved hjelp av metadata view

--- javascript/metadataScript.js
+++ javascript/metadataScript.js
@@ -3,12 +3,15 @@ $(".activeButton").bind("click", function () {
   $(".openDiv").hide();
   var divId = $(this).attr("divId");
   $("#" + divId).show();
+  document.getElementById("metadataTitle").innerHTML = divId;
 });
 
 $(".back-button").bind("click", function () {
   var divId = $(this).attr("divId");
   $("#" + divId).hide();
   $("#showall").show();
+  document.getElementById("metadataTitle").innerHTML =
+    "Metadata &amp; additional services";
 });
 
 $(".sidebarMetadataClosed").bind("click", function () {
