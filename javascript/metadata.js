92eb1fceb9c4da2e63d91e6a3ba91061d94badf2
Maud <johansson.maud98@gmail.com>
2022-05-06 18:49:52 +0200

    merged in metadata

--- javascript/metadata.js
+++ javascript/metadata.js
@@ -9,7 +9,11 @@ class Metadata extends HTMLElement {
     <div class="closeMetadata" id="closeMetadata">
     <img width="20" height="20" src="https://cdn-icons-png.flaticon.com/512/271/271228.png" alt="" title="" >
     </div>
-      <h4 id="metadataTitle">Metadata &amp; additional services</h4>
+    <div style="margin-bottom:5%">
+      <h4 style="float:left" id="metadataTitle">Metadata &amp; additional services</h4>
+      <button class="back-button" id="back-button" divId="Info">Back</button>
+  
+      </div>
       <div class="button-group" id="showall">
         <!--javascript:void(0) if the redirection path is undefined the browser stays on the same page -->
         <button class="activeButton" href="javascript:void(0);" divId="Info">
@@ -18,22 +22,23 @@ class Metadata extends HTMLElement {
         <button class="activeButton" href="javascript:void(0);" divId="Caption">
           Captions
         </button>
-        <button class="activeButton" href="javascript:void(0);" divId="Quotes">
-          Quotes
+        <button class="activeButton" href="javascript:void(0);" divId="Keyword">
+          Keywords
         </button>
       </div>
       <div class="info" class="open_Div" id="Info">
         <div id="info-content"> 
-         </div>
-        <button class="back-button" divId="Info">Back</button>
+        </div>
       </div>
       <div class="caption" class="open_Div" id="Caption">
       <div id="caption-content"> 
       </div>
-        <button class="back-button" divId="Caption">Back</button>
+
       </div>
-      <div class="quotes" class="open_Div" id="Quotes">
-      <button class="back-button" divId="Quotes">Back</button>
+      <div class="keyword" class="open_Div" id="Keyword">
+      <div id="keyword-content"> 
+      </div>
+
     </div>
     </div>
   
--- javascript/metadataScript.js
+++ javascript/metadataScript.js
@@ -1,7 +1,10 @@
+//script for activating the different metadata divs
 $(".activeButton").bind("click", function () {
   $("#showall").hide();
   $(".openDiv").hide();
+  $("#back-button").show();
   var divId = $(this).attr("divId");
+  $("#back-button").attr("divId", divId);
   $("#" + divId).show();
   document.getElementById("metadataTitle").innerHTML = divId;
 });
@@ -9,11 +12,13 @@ $(".activeButton").bind("click", function () {
 $(".back-button").bind("click", function () {
   var divId = $(this).attr("divId");
   $("#" + divId).hide();
+  $("#back-button").hide();
   $("#showall").show();
   document.getElementById("metadataTitle").innerHTML =
     "Metadata &amp; additional services";
 });
 
+//script for closing and opening the metadataview
 $(".sidebarMetadataClosed").bind("click", function () {
   $("#arrow-button").hide();
   $("#metadata").show();
